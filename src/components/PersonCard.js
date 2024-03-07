daaf8ea372d5ae7c83004b9074e70575e3c86c36
paivadul <dul.paiva.comunicaciones@gmail.com>
2024-03-07 01:30:38 -0300

    Pasando a componentes funncionalesa

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,29 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
+const PersonCard = props => {
+    const [age, setAge] = useState(props.age);
 
-export class PersonCard extends Component {
-    constructor(props) {
-        super(props);
-        this.firstName = props.firstName;
-        this.lastName = props.lastName;
-        this.age = props.age;
-        this.haircolor = props.haircolor;
+    const handleClick = () => {
+        setAge(age + 1);
     }
 
-    handleClick = () => {
-        this.age++; // Incrementar la edad
-        this.forceUpdate(); // Forzar la actualización del componente
-    }
-    
-    render() {
-        return(
-            <div>
-                <h1>{this.firstName}, {this.lastName}</h1>
-                <p>Age: {this.age}</p>
-                <p>Hair color: {this.haircolor}</p>
+    return(
+        <div>
+            <h1>{props.firstName}, {props.lastName}</h1>
+            <p>Age: {age}</p>
+            <p>Hair color: {props.haircolor}</p>
 
-                <button onClick={this.handleClick} >Cumpleaños de {this.firstName} {this.lastName}</button>
-            </div>
-        );
-    }
+            <button onClick={handleClick} >Cumpleaños de {props.firstName} {props.lastName}</button>
+        </div>
+    )
 }
+
+export default PersonCard;
+
+// export class PersonCard extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.firstName = props.firstName;
+//         this.lastName = props.lastName;
+//         this.age = props.age;
+//         this.haircolor = props.haircolor;
+//     }
+
+//     handleClick = () => {
+//         this.age++; // Incrementar la edad
+//         this.forceUpdate(); // Forzar la actualización del componente
+//     }
+    
+//     render() {
+//         return(
+//             <div>
+//                 <h1>{this.firstName}, {this.lastName}</h1>
+//                 <p>Age: {this.age}</p>
+//                 <p>Hair color: {this.haircolor}</p>
+
+//                 <button onClick={this.handleClick} >Cumpleaños de {this.firstName} {this.lastName}</button>
+//             </div>
+//         );
+//     }
+// }
